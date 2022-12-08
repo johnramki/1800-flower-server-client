81a80843b1815ef9a5402f063b332813e4830812
Ramki <30864805+johnramki@users.noreply.github.com>
2022-12-08 18:20:51 +0530

    Update Posts.js

--- client/src/component/Posts.js
+++ client/src/component/Posts.js
@@ -1,10 +1,8 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import styles from './post.module.css';
 
 const Post = (props) => {
     const item = props.item;
